33331978922119870b61a17916a4b4040f224e3a
Ivan Demidov <Scrum@list.ru>
2019-12-11 10:16:53 +0300

    perf: add simple linting js and actions (#9)
    * perf: add simple linting js
    
    * Add some rules to eslint; feed linter

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "quotes": ["error", "single"],
+        "indent": ["error", 2]
+    }
+};
